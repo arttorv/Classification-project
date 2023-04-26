16ada7b6d25f64aa25619c8071db22e3e4f0e227
arttorv <62724183+arttorv@users.noreply.github.com>
2023-04-26 17:13:18 +0200

    first steps

--- iris.py
+++ iris.py
@@ -1,7 +1,39 @@
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import io
+from timeit import default_timer as timer
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import accuracy_score
 
-iris = 4 + 5
-print(iris)
+def main():
+    
+    alpha = 1
+    X_train, X_test, t_train, t_test = separateData()
 
+
+
+
+    print(t_train)
+
+
+# FUNCTIONS
+def separateData():
+    iris = load_iris()
+
+    X_train, X_test, t_train, t_test = train_test_split(iris.data, iris.target, test_size=0.5)
+    return X_train, X_test, t_train, t_test
+
+def findMSE(): 
+
+    mse = []
+    grad_mse = []
+
+
+    return mse, grad_mse
+
+main()
 
 
