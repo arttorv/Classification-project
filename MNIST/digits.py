6f18e9a42db89d78984bfcb8febedc05a2bc0560
arttorv <62724183+arttorv@users.noreply.github.com>
2023-04-28 10:25:52 +0200

    project 2 added

--- MNIST/digits.py
+++ MNIST/digits.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib as plt
 
+
+
+def euclidian(x, y):
+    distance = np.sqrt(np.sum((x - y) ** 2))
+    return distance
+
+def nearest_neighbor(x, y):
+    distance = (x - y).T(x - y)
+    return distance
+
